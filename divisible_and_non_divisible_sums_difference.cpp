4ca093c402be974c65f1ca55411bb073fe70e472
srivarsha1372 <113756160+srivarsha1372@users.noreply.github.com>
2025-05-27 17:52:16 +0530

    Create divisible_and_non_divisible_sums_difference.cpp

---
+++ divisible_and_non_divisible_sums_difference.cpp
@@ -0,0 +1,11 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        s1 = 0
+        s2 = 0
+        for i in range(1,n+1):
+            if i % m == 0:
+                s2 += i
+            else:
+                s1 += i
+        return s1 - s2
+        
